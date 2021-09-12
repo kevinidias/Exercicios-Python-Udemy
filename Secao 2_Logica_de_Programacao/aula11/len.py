8ccbb2dec71421400b28d3b111ca40566bc3eb96
kevinidias <kevin.dias.20160@gmail.com>
2021-09-12 20:42:16 -0300

    secao2

---
+++ Secao 2_Logica_de_Programacao/aula11/len.py
@@ -0,0 +1,19 @@
+string1 = input('Digite alguma coisa ')
+string2 = input('Digite outra coisa ')
+
+print(f'A quantidade total de caracteres digitados foi {len(string1) + len(string2)}')
+
+
+
+
+"""
+usuario = input('Digite seu usuário: ')
+qtd_caracteres = len(usuario)
+
+#print(usuario, qtd_caracteres, type(qtd_caracteres))
+
+if qtd_caracteres < 6:
+    print('Você precisa digitar pelo menos 6 caracretes')
+else:
+    print('Você foi cadastrado no sistema.')
+"""
