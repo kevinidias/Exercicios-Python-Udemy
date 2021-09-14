6907906b61d64766dda46926ef7dba4e30bd617c
kevinidias <kevin.dias.20160@gmail.com>
2021-09-14 20:07:53 -0300

    secao2

---
+++ Secao 2_Logica_de_Programacao/aula17/indicesfatiamento.py
@@ -0,0 +1,16 @@
+"""
+Manipulando strings
+    String indices
+    Funções built-in len, abs, type, print, etc...
+    Essas funções podem ser usadas diretamente em cada tipo.
+ """
+
+texto = 'Python'
+
+print(texto[:6])
+print(texto[0:])
+print(texto[2:6])
+print(texto[-1])
+print(texto[0])
+print(texto[0:6:2]) #pula de 2 em 2
+
