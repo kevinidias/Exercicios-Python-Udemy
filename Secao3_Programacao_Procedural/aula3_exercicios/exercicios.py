e651a5e2abc87204d89c49ce3a7041368008ac37
kevinidias <kevin.dias.20160@gmail.com>
2021-10-22 21:11:07 -0300

    exercicios propostos funcao

---
+++ Secao3_Programacao_Procedural/aula3_exercicios/exercicios.py
@@ -0,0 +1,9 @@
+"""
+1- Crie uma função que exibe uma saudação com os parâmetros saudação e nome.
+
+2- Cria uma função que recebe 3 números como parêmetros e exiba a soma entre eles.
+
+3- Crie uma função que receba 2 números. O primeiro é um valor e o segundo um percentual (ex. 10%). Retorn (return) o valor do primeiro número somado do aumento do percentual do mesmo. 
+
+4- Fizz Buzz - Se o parâmetro da função for divisível por 2, retorne fizz, se o parâmetro da função for divisível por 5, retorne buzz. Se o parâmetro da função for divisível por 5 e por 3, retorne FizzBuzz, caso contrário, retorne o número enviado.
+"""
