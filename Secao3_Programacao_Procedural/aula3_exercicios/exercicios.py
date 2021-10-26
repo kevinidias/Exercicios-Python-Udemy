67bd1ed5ba022b47880ab7ef7bccd3c734a496c6
kevinidias <kevin.dias.20160@gmail.com>
2021-10-26 13:38:06 -0300

    exercicio 3 e 4

--- Secao3_Programacao_Procedural/aula3_exercicios/exercicios.py
+++ Secao3_Programacao_Procedural/aula3_exercicios/exercicios.py
@@ -5,7 +5,7 @@
 
 3- Crie uma função que receba 2 números. O primeiro é um valor e o segundo um percentual (ex. 10%). Retorne (return) o valor do primeiro número somado do aumento do percentual do mesmo. 
 
-4- Fizz Buzz - Se o parâmetro da função for divisível por 2, retorne fizz, se o parâmetro da função for divisível por 5, retorne buzz. Se o parâmetro da função for divisível por 5 e por 3, retorne FizzBuzz, caso contrário, retorne o número enviado.
+4- Fizz Buzz - Se o parâmetro da função for divisível por 3, retorne fizz, se o parâmetro da função for divisível por 5, retorne buzz. Se o parâmetro da função for divisível por 5 e por 3, retorne FizzBuzz, caso contrário, retorne o número enviado.
 """
 
 
@@ -27,3 +27,23 @@ print(soma)
 
 
 #3
+def aumento_percentual(valor, percentual):
+    return valor + (valor * percentual / 100)
+
+ap = aumento_percentual(50, 10)
+print(ap)
+
+#4
+def fb(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return 'fizzbuzz'
+    if n % 5 == 0:
+        return 'buzz'
+    if n % 3 == 0:
+        return 'fizz'
+    return n
+
+print(fb(7))
+print(fb(10))
+print(fb(15))
+print(fb(22))
